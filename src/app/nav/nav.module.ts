6e94fdcccec462da86eeb4115bd3af439d13ca93
shubham chaudhari <Mahavir@DESKTOP-A1USG6O>
2025-08-21 23:04:23 +0530

    Nav BAr created by Shubham

---
+++ src/app/nav/nav.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NavRoutingModule } from './nav-routing.module';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+
+
+@NgModule({
+  declarations: [
+    NavBarComponent
+  ],
+  imports: [
+    CommonModule,
+    NavRoutingModule
+  ]
+})
+export class NavModule { }
